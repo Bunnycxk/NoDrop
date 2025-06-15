f712417dfccb2d42a3509025ca2a37e16fc3dd9e
jeshrz <jeshrz@foxmail.com>
2025-06-15 15:23:19 +0800

    fix: resolve strip for consumer

--- kmodule/trace.c
+++ kmodule/trace.c
@@ -148,7 +148,7 @@ TRACEPOINT_PROBE(syscall_exit_probe, struct pt_regs *regs, long ret)
         // end = nod_nsecs();
         // pr_info("post %llu\n", end - start);
         nod_proc_set_out(p);
-        vpr_info("ctxswtich ts:%llu\n", nod_rdtsc() - p->buffer.info->tsc);
+        // vpr_info("ctxswtich ts:%llu\n", nod_rdtsc() - p->buffer.info->tsc);
 
         break;
 
